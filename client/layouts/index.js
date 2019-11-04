7a0fb365a25c21dd1459f3d9f8d9c5914b216cb5
ElisaHery <elisa.hery@gmail.com>
2019-11-04 15:02:16 +0100

    move assets to static folder

--- client/layouts/index.js
+++ client/layouts/index.js
@@ -1,7 +1,12 @@
-import Header from "./components/Header";
+import Header from './components/Header';
+import Head from 'next/head';
 
 const Layout = props => (
   <div>
+    <Head>
+      <title>Brasserie 3ienchs_favicon</title>
+      <link rel='shortcut icon' href='./../static/logos/3ienchs_favicon.gif' />
+    </Head>
     <Header />
     {props.children}
   </div>
--- client/src/pages/Order/index.js
+++ client/src/pages/Order/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Order = props => <div className='hero mt-30'>Je suis la page Order</div>;
+const Order = props => (
+  <div className='hero mt-15 bg-base w-full h-100-vh'>
+    Je suis la page Order
+  </div>
+);
 
 export default Order;
