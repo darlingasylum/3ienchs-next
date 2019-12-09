2920fbc7d43e28a3f218ba66238f272275587c6f
ElisaHery <elisa.hery@gmail.com>
2019-12-09 16:18:55 +0100

    add calculate price util - display total price in basket component

--- client/src/pages/Order/components/Basket/index.js
+++ client/src/pages/Order/components/Basket/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
+import { calculatePrice } from './../../../../../utils/calculatePrice';
 import bottles from './../../../../../static/icons/bottles.png';
 
-const Basket = props => {
+const basketSelector = state => state.basket.articles;
+
+const Basket = () => {
+  let basket = useSelector(basketSelector);
+
+  const unitBeerPrice = 3;
+
+  const countArticles = basket => {
+    let count = 0;
+    if (basket.length > 0) {
+      basket.map(product => (count = count + product.quantity));
+    }
+    return count;
+  };
+
   return (
-    <div className='fixed r-10 t-15 bg-grey-light p-6 br-40 flex flex-col align-center'>
+    <div className='fixed r-10 t-15 background-greylight-transp p-6 br-40 flex flex-col align-center'>
       <img
         src={bottles}
         alt='logo_iench'
         className='max-width-50 w-25-percent'
       />
       <div className='nickname'>
-        <span>0</span>
+        <span>{countArticles(basket)}</span>
         <span>=</span>
-        <span>0</span>
+        <span>{calculatePrice(countArticles(basket), unitBeerPrice)}</span>
         <span>€</span>
       </div>
       <div className='nickname text-white bg-purple-dark br-8 pl-1 pr-1 cursor-pointer'>
