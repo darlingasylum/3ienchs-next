03a96eb2ecb4e81f18dccb02ccd257b72f9e519c
ElisaHery <elisa.hery@gmail.com>
2019-12-10 12:43:32 +0100

    move link component folder to src/components

--- client/layouts/components/Header/Navbar.js
+++ client/layouts/components/Header/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import { Link, NavLink } from "react-router-dom"
-import Link from './components/Link';
+import Link from '../../../src/components/Link';
 
 // import { connect } from "react-redux";
 
