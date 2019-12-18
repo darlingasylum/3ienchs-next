36f4de0ecebfbf51aea3d23ecac84572f8df092e
ElisaHery <elisa.hery@gmail.com>
2019-12-18 16:43:24 +0100

    add jest and babel config to set up tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,17 +1,21 @@
 module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js'],
-  transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest'
-  },
-  testMatch: ['**/*.(test|spec).(js|tsx)'],
-
+  // transform: {
+  //   '^.+\\.tsx?$': 'ts-jest'
+  // },
+  testMatch: ['**/*.(test|spec).(ts|tsx|js)'],
+  // globals: {
+  //   'ts-jest': {
+  //     useBabelrc: true,
+  //     tsConfigFile: 'jest.tsconfig.json'
+  //   }
+  // },
   coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js'],
   setupFilesAfterEnv: ['<rootDir>/enzyme.js'],
-  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/mocks.js',
-    '\\.(css|less|scss)$': '<rootDir>/__mocks__/mocks.js'
-  },
-  verbose: true
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary']
+  // moduleNameMapper: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>/__mocks__/mocks.js',
+  //   '\\.(css|less|scss)$': '<rootDir>/__mocks__/mocks.js'
+  // }
 };
