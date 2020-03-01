506b4210c8350c2cb81583f820459ab96ac4061b
ElisaHery <elisa.hery@gmail.com>
2020-03-01 19:03:26 +0100

    Feat: add back office EditForm

--- client/src/pages/AdminDashboard/components/EditProduct/EditForm/index.js
+++ client/src/pages/AdminDashboard/components/EditProduct/EditForm/index.js
@@ -1,119 +1,185 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
+
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Button from '@material-ui/core/Button';
+
 import { withStyles } from '@material-ui/core/styles';
 
 const EditForm = ({ product, handleSubmit }) => {
   const {
     product_name,
     product_type,
-    product_id,
-    product_bg,
     product_price,
     product_proof,
+    product_descr,
     product_img,
-    bg_color,
+    product_bg,
     title_color,
     text_color,
+    featuring,
+    partner,
     product_stock,
-    featuring
+    product_id
   } = product;
 
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const formik = useFormik({
-    initialValues: {
-      name: product_name,
-      type: product_type,
-      price: product_price + '€',
-      proof: product_proof,
-      background: product_bg,
-      image: product_img,
-      titleColor: title_color,
-      textColor: text_color,
-      stock: product_stock,
-      featuring: featuring
-    },
-    onSubmit: handleSubmit
-  });
-
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className='flex flex-col'>
-        <div className='flex justify-between flex-col-md'>
-          <StyledTextField
-            id='name'
-            name='name'
-            type='name'
-            label='Nom'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.name}
-          />
-          <StyledTextField
-            id='type'
-            name='type'
-            type='type'
-            label='Type'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.type}
-          />
-        </div>
-        <div className='flex justify-between flex-col-md'>
-          <StyledTextField
-            id='proof'
-            name='proof'
-            type='proof'
-            label='Degré'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.proof}
-          />
-          <StyledTextField
-            id='price'
-            name='price'
-            type='price'
-            label='Prix'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.price}
-          />
-        </div>
+    <div>
+      <Formik
+        initialValues={{
+          name: product_name,
+          type: product_type,
+          price: product_price,
+          proof: product_proof,
+          description: product_descr,
+          image: product_img,
+          background: product_bg,
+          titleColor: title_color,
+          textColor: text_color,
+          featuring: featuring,
+          partner: partner,
+          stock: product_stock
+        }}
+        // validate={values => {
+        //   const errors = {};
+        //   if (!values.email) {
+        //     errors.email = 'Required';
+        //   } else if (
+        //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        //   ) {
+        //     errors.email = 'Invalid email address';
+        //   }
+        //   return errors;
+        // }}
+        onSubmit={handleSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting
+          /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <div className='flex flex-col'>
+              <div className='flex justify-between flex-col-md'>
+                <StyledTextField
+                  id='name'
+                  name='name'
+                  type='name'
+                  label='Nom'
+                  variant='outlined'
+                  margin='normal'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                />
+                {/* {errors.email && touched.email && errors.email} */}
+                <StyledTextField
+                  id='type'
+                  name='type'
+                  type='type'
+                  label='Type'
+                  variant='outlined'
+                  margin='normal'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.type}
+                />
+                {/* {errors.password && touched.password && errors.password} */}
+              </div>
 
-        <div className='flex justify-between flex-col-md'>
-          <StyledTextField
-            id='featuring'
-            name='featuring'
-            type='featuring'
-            label='Featuring ?'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.featuring}
-          />
-          <StyledTextField
-            id='stock'
-            name='stock'
-            type='stock'
-            label='Stock'
-            variant='outlined'
-            margin='normal'
-            onChange={formik.handleChange}
-            value={formik.values.stock}
-          />
-        </div>
+              <div className='flex justify-between flex-col-md'>
+                <StyledTextField
+                  id='price'
+                  name='price'
+                  type='price'
+                  label='Prix (€)'
+                  variant='outlined'
+                  margin='normal'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.price}
+                />
+                <StyledTextField
+                  id='proof'
+                  name='proof'
+                  type='proof'
+                  label='Degré'
+                  variant='outlined'
+                  margin='normal'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.proof}
+                />
+              </div>
 
-        <Button type='submit' variant='contained' color='secondary'>
-          Modifier
-        </Button>
-      </div>
-    </form>
+              <div className='flex justify-between flex-col-md'>
+                <StyledTextField
+                  id='description'
+                  name='description'
+                  type='description'
+                  label='Degré'
+                  variant='outlined'
+                  margin='normal'
+                  multiline
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.description}
+                />
+              </div>
+
+              <div className='flex justify-between flex-col-md'>
+                <FormControlLabel
+                  control={
+                    <StyledCheckbox
+                      name='featuring'
+                      checked={values.featuring}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                  }
+                  label='Featuring ?'
+                />
+                <StyledTextField
+                  id='partner'
+                  name='partner'
+                  type='partner'
+                  label='Partenaire de featuring'
+                  variant='outlined'
+                  margin='normal'
+                  disabled={!values.featuring}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.partner}
+                />
+              </div>
+              <div className='flex justify-between flex-col-md'>
+                <StyledTextField
+                  id='stock'
+                  name='stock'
+                  type='stock'
+                  label='Stock'
+                  variant='outlined'
+                  margin='normal'
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.stock}
+                />
+              </div>
+              <StyledButton type='submit' variant='contained' color='secondary'>
+                Modifier
+              </StyledButton>
+            </div>
+          </form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
@@ -125,3 +191,15 @@ const StyledTextField = withStyles({
     margin: '0.5rem'
   }
 })(TextField);
+
+const StyledCheckbox = withStyles({
+  root: {
+    marginLeft: '0.5rem'
+  }
+})(Checkbox);
+
+const StyledButton = withStyles({
+  root: {
+    margin: '0.5rem'
+  }
+})(Button);
--- client/src/pages/AdminDashboard/components/EditProduct/index.js
+++ client/src/pages/AdminDashboard/components/EditProduct/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import EditForm from './EditForm/index';
+import { APICall } from '../../../../../utils/APICall';
 
 const EditProduct = ({ product }) => {
   const handleSubmit = values => {
     alert(JSON.stringify(values, null, 2));
+
+    // TODO;
+    // const fetch_param = {
+    //   method: 'POST',
+    //   headers: { 'content-type': 'application/json' },
+    //   body: JSON.stringify(values)
+    // };
+
+    // APICall(`http://localhost:4000/api/products/update`, fetch_param)
+    //   .then(response => {
+    //     console.log('response -->', response);
+    //     return response;
+    //   })
+    //   .catch(err => console.log(err.message));
   };
+
+  const booleanFeaturingValue = product.featuring === 0 ? false : true;
+
+  const updatedProduct = { ...product, featuring: booleanFeaturingValue };
 
   return (
     <div className='w-half-md m-20'>
-      <h2 className='mt-10'>Modifier la {product.product_name} :</h2>
-      <EditForm product={product} handleSubmit={handleSubmit}></EditForm>
+      <h2 className='mt-10'>Modifier la {updatedProduct.product_name} :</h2>
+      <EditForm product={updatedProduct} handleSubmit={handleSubmit}></EditForm>
     </div>
   );
 };
