a4b8235be5a7a51b2ab60a30986a4a3798c38934
ElisaHery <elisa.hery@gmail.com>
2020-05-01 18:59:27 +0200

    feat: refacto Step style

--- client/src/pages/BasketPage/components/Step/index.js
+++ client/src/pages/BasketPage/components/Step/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Step = ({ number }) => {
-  return (
-    <div className='bg-black text-white nickname br-100 h-10 w-10 flex align-center justify-center inline-flex mr-4 mb-2'>
-      {number}
-    </div>
-  );
+  return <div className='step'>{number}</div>;
 };
 
 export default Step;
